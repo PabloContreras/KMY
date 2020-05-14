25b75bd94006a6e924997aecc82e40e28bd3e9b0
pablocontreras <pablo_contreras_1997@outlook.com>
2020-05-14 13:55:06 -0500

    video changed

--- resources/views/salon/videos.blade.php
+++ resources/views/salon/videos.blade.php
@@ -4,7 +4,7 @@
     <div class="row text-center">            
         <div class="col-md-12">
             <div class="card">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/KeIWlIo2qVg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/rUBaeuc6JeI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
         </div>                
     </div>
